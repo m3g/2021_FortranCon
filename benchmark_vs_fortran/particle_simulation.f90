b3a5ca97a5d131fccc82e4510258cab88b6713af
Leandro Martinez <leandro@iqm.unicamp.br>
2021-09-15 11:31:27 -0300

    modernized syntax and made it dimension-generic

--- benchmark_vs_fortran/particle_simulation.f90
+++ benchmark_vs_fortran/particle_simulation.f90
@@ -9,38 +9,54 @@ double precision function wrap(x,side)
     end if
 end function wrap
 
-subroutine force_pair(fpair,x,y,cutoff,side)
+double precision function norm(ndim,x)
+    integer :: ndim
+    double precision :: x(ndim)
+    norm = 0.d0
+    do i = 1, ndim
+        norm = norm + x(i)**2
+    end do
+    norm  = dsqrt(norm)
+end function norm
+
+subroutine force_pair(ndim,fpair,x,y,cutoff,side)
     implicit none
-    integer :: i
-    double precision :: fpair(2), wrap, d
-    double precision :: x(2), y(2), cutoff, side, dv(2)
-    do i = 1,2
+    integer :: i, ndim
+    double precision :: fpair(ndim), wrap, d, norm
+    double precision :: x(ndim), y(ndim), cutoff, side, dv(ndim)
+    do i = 1, ndim
         dv(i) = wrap(y(i) - x(i), side)
     end do
-    d = dsqrt(dv(1)**2 + dv(2)**2)
+    d = norm(ndim,dv)
     if (d > cutoff) then
-        fpair(1) = 0.d0
-        fpair(2) = 0.d0
+        do i = 1, ndim
+            fpair(i) = 0.d0
+        end do
     else
         dv = dv / d
-        do i = 1,2
+        do i = 1, ndim
            dv(i) = (dv(i)/d)*(d-cutoff)**2
         end do
         fpair = dv
     end if
 end subroutine force_pair
 
-subroutine forces(n,f,x,cutoff,side)
+subroutine forces(n,ndim,f,x,cutoff,side)
     implicit none
-    integer :: n, i, j
-    double precision :: f(2,n), x(2,n)
-    double precision :: fpair(2)
+    integer :: n, ndim, i, j
+    double precision :: f(ndim,n), x(ndim,n)
+    double precision :: fpair(ndim)
     double precision :: cutoff, side
+    do i = 1, n
+        do j = 1, ndim
+            f(j,i) = 0.d0
+        end do
+    end do
     do i = 1, n-1
         do j = i+1, n
-            call force_pair(fpair,x(:,i),x(:,j),cutoff,side)
-            f(1,i) = f(1,i) - fpair(1)
-            f(2,j) = f(2,i) + fpair(2)
+            call force_pair(ndim,fpair,x(:,i),x(:,j),cutoff,side)
+            f(:,i) = f(:,i) - fpair
+            f(:,j) = f(:,j) + fpair
         end do
     end do
 end subroutine forces
@@ -49,23 +65,22 @@ double precision function dble_rand()
     call random_number(dble_rand)
 end function dble_rand
 
-subroutine md(n,x0,v0,mass,dt,nsteps,isave,trajectory,cutoff,side)
+subroutine md(n,ndim,x0,v0,mass,dt,nsteps,isave,trajectory,cutoff,side)
     implicit none
-    integer :: n, i, j, k, step, nsteps, isave, isaved
+    integer :: n, ndim, i, j, k, step, nsteps, isave, isaved
     double precision :: dt
-    double precision :: x0(2,n), v0(2,n), mass(n)
-    double precision :: x(2,n), v(2,n), f(2,n), a(2,n)
-    double precision :: trajectory(2,n,nsteps/isave+1)
+    double precision :: x0(ndim,n), v0(ndim,n), mass(n)
+    double precision :: x(ndim,n), v(ndim,n), f(ndim,n), a(ndim,n)
+    double precision :: trajectory(ndim,n,nsteps/isave+1)
     double precision :: cutoff, side
-
     ! Save initial positions
-    trajectory(:,:,1) = x
+    trajectory(:,:,1) = x0
     x = x0
     v = v0
     isaved = 1
     do step = 1, nsteps
         ! Compute forces
-        call forces(n,f,x,cutoff,side)
+        call forces(n,ndim,f,x,cutoff,side)
         ! Update positions and velocities 
         do i = 1, n
            a(:,i) = f(:,i) / mass(i)
@@ -82,36 +97,30 @@ end subroutine md
 
 program main
     implicit none
-    integer, parameter :: n = 100
+    integer, parameter :: n = 100, ndim = 2
     integer :: i, j, k, nsteps, isave
-    double precision :: x0(2,n), v0(2,n), mass(n)
+    double precision :: x0(ndim,n), v0(ndim,n), mass(n)
     double precision :: dt
     double precision :: cutoff, side, wrap
     double precision, allocatable :: trajectory(:,:,:)
     double precision :: dble_rand
-
     ! Initialize positions and velocities
     do i = 1, n
-        do j = 1, 2
+        do j = 1, ndim
             x0(j,i) = -50 + 100*dble_rand()
-            x0(j,i) = -50 + 100*dble_rand()
-            v0(j,i) = -1 + 2*dble_rand()
             v0(j,i) = -1 + 2*dble_rand()
         end do
         mass(i) = 1.d0
     end do
-
     ! Parameters
     dt = 0.1
     nsteps = 50000
     isave = 1000
     cutoff = 5.
     side = 100.
-    allocate(trajectory(2,n,nsteps/isave + 1))
-
+    allocate(trajectory(ndim,n,nsteps/isave + 1))
     ! Run simulation
-    call md(n,x0,v0,mass,dt,nsteps,isave,trajectory,cutoff,side)
-
+    call md(n,ndim,x0,v0,mass,dt,nsteps,isave,trajectory,cutoff,side)
     open(10,file="traj_fortran.xyz")
     k = 0
     do i = 1, nsteps/isave + 1
@@ -123,6 +132,5 @@ program main
         end do
     end do
     close(10)
-
 end program main
 
